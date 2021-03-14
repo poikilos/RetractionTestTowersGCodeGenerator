411df36334b682589490877c35dd6f7bddfc6b43
poikilos <7557867+poikilos@users.noreply.github.com>
2021-03-14 10:59:09 -0400

    Improve use of PEP8. Uncomment parts not understood by pycodetool. Change pycodetool-output to md.

--- Program.py
+++ Program.py
@@ -1,21 +1,21 @@
-﻿from System import *
-# Processed by pycodetool https://github.com/poikilos/pycodetool2021-03-14 10:52:20
+﻿# Processed by pycodetool https://github.com/poikilos/pycodetool2021-03-14 10:52:20
+# from System import *
+# from System.Collections.Generic import *
+# from System.IO import *
+# from System.Linq import *
 import sys
-#from System.Collections.Generic import *
-#from System.IO import *
-#from System.Linq import *
+
 
 class Program:
     def __init__(self):
         self._FirstTowerZ = 2.1m
 
     class CurvePointType:
-#        def __init__(self):
-#
+        def __init__(self):
+
     class CurvePoint:
-#        def __init__(self):
-#
-#    def GetTemplateReader():
-#        pass
-#
-    GetTemplateReader = staticmethod(GetTemplateReader)
+        def __init__(self):
+
+    @classmethod
+    def GetTemplateReader(cls):
+        pass
