296d78ca957cd0624b11c6197246326f1916c3fc
poikilos <7557867+poikilos@users.noreply.github.com>
2021-03-14 10:55:40 -0400

    Run pycodetool on Program.py to remove IronPython.

--- Program.py
+++ Program.py
@@ -1,19 +1,21 @@
 ﻿from System import *
-from System.Collections.Generic import *
-from System.IO import *
-from System.Linq import *
+# Processed by pycodetool https://github.com/poikilos/pycodetool2021-03-14 10:52:20
+import sys
+#from System.Collections.Generic import *
+#from System.IO import *
+#from System.Linq import *
 
-class Program(object):
+class Program:
     def __init__(self):
         self._FirstTowerZ = 2.1m
 
-    class CurvePointType(object):
-        def __init__(self):
-
-    class CurvePoint(object):
-        def __init__(self):
-
-    def GetTemplateReader():
-        pass
-
+    class CurvePointType:
+#        def __init__(self):
+#
+    class CurvePoint:
+#        def __init__(self):
+#
+#    def GetTemplateReader():
+#        pass
+#
     GetTemplateReader = staticmethod(GetTemplateReader)
